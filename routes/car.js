fcdcafbd208acc76a3a8e98c6ba4449c4478818c
Lety7 <30472838+Lety7@users.noreply.github.com>
2019-07-11 20:34:32 -0500

    Update car.js

--- routes/car.js
+++ routes/car.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const api = express.Router();
 
-const { getAllCars, getBrandById,changeOwner} = require ('../operations/API/car.api');
+const { getAllCars, getBrandById,changeOwner,getAllCarsByBrands,getAllCarsByModels} = require ('../operations/API/car.api');
 
 api.route('')
     .get(getAllCars)
@@ -12,9 +12,9 @@ api.route('/:id')
 
 
 api.route('/brands/:nameBrand')
-    .get(getAllCars);
+    .get(getAllCarsByBrands);
 
 api.route('/models/:nameModel')
-    .get(getAllCars);
+    .get(getAllCarsByModels);
 
 module.exports = api;
