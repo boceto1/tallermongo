d5be1ffc95e6befaef53164339d311c731ef61d9
Lety7 <30472838+Lety7@users.noreply.github.com>
2019-07-11 20:30:38 -0500

    Segundo Commit

---
+++ operations/API/brand.api.js
@@ -0,0 +1,11 @@
+export const getAllBrands = (req, res) => {
+    res.send("Works well");
+}
+
+export const createBrand = (req, res) => {
+    res.send("Works well");
+}
+
+export const getBrandById = (req, res) => {
+    res.send("Works well");
+}
---
+++ operations/API/car.api.js
@@ -0,0 +1,11 @@
+export const getAllCars = (req, res) => {
+    res.send("Works well");
+}
+
+export const getBrandById = (req, res) => {
+    res.send("Works well");
+}
+
+export const changeOwner = (req, res) => {
+    res.send("Works well");
+}
